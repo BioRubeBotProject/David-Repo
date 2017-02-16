0d1b7daa374f84317155470516bad712c858ec9c
David Brosnan <dbrosnan07@gmail.com>
2017-02-16 04:55:47 -0600

    TimeScale fix for level one + compile error fix
    This fixes the time control buttons for level 1 including the
    highlighting for the fast forward button.  I also fixed the compile
    error for the "Tutorial Button" by adding a temporary "fix" button
    outside of the main camera inside level 1 scene.  This must be there for
    now because the Object "Tutorial Button" is part of a script that is
    also used in the main menu scene.  You can NOT delete, comment or make
    this object private otherwise it will cause compile errors when working
    in the main menu scene.

---
+++ Assets/Scripts/TimeScale.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class TimeScale : MonoBehaviour
+{
+	private static float saveTimeScale;
+
+
+	public void Start()
+	{
+		Time.timeScale = 0;
+
+        Debug.Log("Start -> " + Time.timeScale);
+    }
+
+
+	public void PlayButton()
+	{
+		//Set TimeScale to 1, regardless of current state
+		Time.timeScale = 1.0f;
+
+        Debug.Log("PlayButton -> " + Time.timeScale);
+    }
+
+
+	public void PauseButton()
+	{
+        //IF game is NOT paused THEN save time and pause game
+        if(Time.timeScale != 0)
+        {
+            saveTimeScale = Time.timeScale;
+            Time.timeScale = 0;     
+        }
+
+        //ELSE return game time to saved time
+        else
+        {
+            Time.timeScale = saveTimeScale;
+        }
+
+        Debug.Log("PauseButton -> " + Time.timeScale);
+    }
+
+
+	public void FastForwardButton()
+    {
+        //IF timeScale is not 2 THEN double speed to 2
+        if(Time.timeScale != 2)
+        {
+            Time.timeScale = 2; 
+        }
+
+        //IF timeScale is 2 THEN restore speed to 1
+        else if(Time.timeScale == 2)
+        {
+            Time.timeScale = 1.0f;
+
+            //Remove highlight from button
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+
+        Debug.Log("FastForwardButton -> " + Time.timeScale);
+    }
+
+
+    public void ResetTime()
+    {
+        saveTimeScale = 0;
+        Time.timeScale = 0;
+
+        Debug.Log("ResetButton -> " + Time.timeScale);
+    }
+
+
+}
